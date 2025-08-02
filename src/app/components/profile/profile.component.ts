fa6bdf182697eebacfb907e5d566935af0fb096a
João Lopes <joaovlopesmartins@gmail.com>
2025-08-01 22:31:56 -0300

    feat: implement profile component with initial layout and user data

---
+++ src/app/components/profile/profile.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+import { SpotifyApiService } from '@/core/services/spotify-api.service';
+
+@Component({
+  selector: 'app-profile',
+  templateUrl: './profile.component.html',
+  styleUrls: ['./profile.component.scss'],
+})
+export class ProfileComponent implements OnInit {
+  user: any = null;
+  playlists: any[] = [];
+
+  constructor(private api: SpotifyApiService) {}
+
+  ngOnInit(): void {
+    const token = localStorage.getItem('spotify_token')!;
+    // busca perfil
+    this.api.getUserProfile(token).subscribe((u) => (this.user = u));
+    // busca playlists
+    this.api
+      .getUserPlaylists(token)
+      .subscribe((pl) => (this.playlists = pl.items));
+
+    console.log(this.user);
+  }
+
+  logoutSpotify() {
+    console.log('Logout');
+  }
+}
